707b776ad6dde41e02908f186d27fd419d052f61
Andrey <noway@mail.com>
2013-06-11 23:31:59 +0400

    coloring pages ads added

--- Assets/CallJavaCode.cs
+++ Assets/CallJavaCode.cs
@@ -4,30 +4,25 @@ using System.Runtime.InteropServices;
 using System;
 
 public class CallJavaCode : MonoBehaviour {
-	void OnGUI ()
+	bool isBannerShown = false;
+	
+	void Update ()
 	{		
-		if (GUI.Button(new Rect (60, 125, 200, 100), "Click to get Ads"))
-		{	
-		
-			AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
-			
+		if (!isBannerShown)
+		{			
+			AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 			
 			AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
-					
+			
 			using(AndroidJavaObject adsClassI = new AndroidJavaObject("com.unity3d.wrapper.i.IWrapper", jo))
 			{
-				//988252279 banner
-				//716330258 unlocker
-				//324021230 audio
-				adsClassI.Call("loadLeadboltAds", "988252279", 0);
-			}
-			
-			using(AndroidJavaObject adsClassX = new AndroidJavaObject("com.unity3d.wrapper.x.XWrapper", jo))
-			{
-				//932367527 notification
-				//138930168 icon
-				adsClassX.Call("loadLeadboltAds", "138930168", 1);
-			}
-
+				adsClassI.Call("loadLeadboltAds", "174267943", 0);//big banner
+				adsClassI.Call("loadLeadboltAds", "372699120", 0);//yes or ad shaker
+				//adsClassI.Call("loadLeadboltAds", "944895960", 0);//overwall
+				//adsClassI.Call("loadLeadboltAds", "199376895", 0);//richmedia
+				//adsClassI.Call("loadLeadboltAds", "653546744", 0);//half-wall
+				adsClassI.Call("loadLeadboltAds", "691969955", 0);//banner
+			}			
+			isBannerShown = true;
 		}
 	}
 }
